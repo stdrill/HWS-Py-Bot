66bbf210996449d7a509d9f71b8a65041a50b4ce
stdrill <tanya23.96@mail.ru>
2022-12-16 19:46:21 +0300

    readme added

--- symbols.py
+++ symbols.py
@@ -1,13 +1,13 @@
 BOT_TOKEN_FILENAME = 'token.txt'
 
-SYMBOL_X = '❌'
-SYMBOL_O = '⭕'
+SYMBOL_X = 'X'
+SYMBOL_O = 'O'
 SYMBOL_UNDEF = '◻'
 
 ANSW_YOUR_TURN = 'Ваш ход'
-ANSW_YOU_WIN = 'Вы победили'
-ANSW_BOT_WIN = 'Победил бот'
+ANSW_YOU_WIN = 'Вы победили!'
+ANSW_BOT_WIN = 'Победил бот!'
 ANSW_DRAW = 'Ничья'
-ANSW_HELP = 'Используйте команду /new_game для начала новой игры'
+ANSW_HELP = 'Используйте команду /new_game для старта новой игры'
 
 ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимите на ' + SYMBOL_UNDEF
